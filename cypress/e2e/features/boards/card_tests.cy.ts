5aae28628b5646ff323294b6bcd985141a9e56d6
Reinaldo <reinaldo.rossetti@outlook.com>
2025-01-08 08:41:36 -0300

    ajustes de codigo

--- cypress/e2e/features/boards/card_tests.cy.ts
+++ cypress/e2e/features/boards/card_tests.cy.ts
@@ -3,19 +3,19 @@ import { faker } from '@faker-js/faker'
 import schema_create_card from "../../../support/schemas/create_card"; // Importe o esquema JSON
 
 // Para criação de um card é necessário passar o id da lista já criada.
-// criei função para criar uma lista e retornar o id da lista. Mas o id do boarding fiz pois o mesmo é limitado.
+// criei função para criar uma lista e retornar o id da lista. Mas o id do boarding está fixo pois o mesmo é limitado.
 // "message": "Cannot create board as organization is at its board limit"
 
 var card_id = 0
-var board_id = "677bdc62b7d77e8c6cbff8e2"
 var list_id = "677bf6c6f04d50086972a5a8"
+const BOARD_ID = "677bdc62b7d77e8c6cbff8e2"
 const API_TOKEN = `${Cypress.env('ACCESS_TOKEN')}`
 const API_KEY = `${Cypress.env('API_KEY')}`
 const nome_card = `${faker.animal.petName()}`
 
 before(() => {
   cy.open_boards()
-  cy.create_a_new_list(board_id, API_TOKEN, API_KEY)
+  cy.create_a_new_list(BOARD_ID, API_TOKEN, API_KEY)
   .then(response => {
     expect(response.status).to.equal(200)
     list_id = response.body.id
@@ -31,6 +31,7 @@ describe('Funcionalidade de Card', () => {
       expect(response.status).to.equal(200)
       expect(response.body.name).to.equal(nome_card)
       expect(response.body.idList).to.equal(list_id)
+      expect(response.body.idBoard).to.equal(BOARD_ID)
       expect(idLength).to.eq(24); // Validar se o id do board tem 24 caracteres.
       // Validar o schema dos dados de retorno do endpoint.
       expect(response.body).to.be.jsonSchema(schema_create_card);
