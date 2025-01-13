7b2e7de2eae92738f874ece5b36dffdab406f03a
Reinaldo <reinaldo.rossetti@outlook.com>
2025-01-13 09:36:01 -0300

    adicionando dados em json, pra enxugar variaveis global nos testes

--- cypress/e2e/features/card_tests.cy.ts
+++ cypress/e2e/features/card_tests.cy.ts
@@ -2,6 +2,7 @@
 import { faker } from '@faker-js/faker'
 import schema_create_card from "../../support/schemas/create_card"; // Importe o esquema JSON
 import schema_get_card from "../../support/schemas/get_card"; // Importe o esquema JSON
+import data_card from '../../fixtures/cards_data.json';  // Dados do card que esta no fixture.
 
 // Para criação de um card é necessário passar o id da lista já criada.
 // criei função para criar uma lista e retornar o id da lista. Mas o id do boarding está fixo pois o mesmo é limitado.
@@ -9,33 +10,29 @@ import schema_get_card from "../../support/schemas/get_card"; // Importe o esque
 
 // Variáveis globais, estão fora do escopo do teste. Em caso de falha, a variável não é resetada.
 var card_id = 0
-var list_id = "677bf6c6f04d50086972a5a8"
-const BOARD_ID = "677bdc62b7d77e8c6cbff8e2"
 const nome_card = `${faker.animal.petName()}`
 const API_TOKEN = `${Cypress.env('ACCESS_TOKEN')}`
 const API_KEY = `${Cypress.env('API_KEY')}`
-var board_name = "Automação Cypress CT001 - Update"
-var description = "Criação de Automação de Testes com Cypress"
 
 before(() => {
   cy.open_boards(API_TOKEN, API_KEY)
-  cy.create_a_new_list(BOARD_ID, API_TOKEN, API_KEY)
+  cy.create_a_new_list(data_card.board_id, API_TOKEN, API_KEY)
     .then(response => {
       expect(response.status).to.equal(200)
-      list_id = response.body.id
+      data_card.list_id = response.body.id
     })
 })
 
 describe('Funcionalidade de Card', () => {
   it('CT001 - Cenário - Criação de um novo Card no Board do Trello - Teste Positivo', () => {
-    cy.create_a_card(list_id, nome_card, API_TOKEN, API_KEY)
+    cy.create_a_card(data_card.list_id, nome_card, API_TOKEN, API_KEY)
       .then(response => {
         card_id = response.body.id
         const idLength = card_id.toString().length
         expect(response.status).to.equal(200)
         expect(response.body.name).to.equal(nome_card)
-        expect(response.body.idList).to.equal(list_id)
-        expect(response.body.idBoard).to.equal(BOARD_ID)
+        expect(response.body.idList).to.equal(data_card.list_id)
+        expect(response.body.idBoard).to.equal(data_card.board_id)
         expect(idLength).to.eq(24); // Validar se o id do board tem 24 caracteres.
         // Validar o schema dos dados de retorno do endpoint.
         expect(response.body).to.be.jsonSchema(schema_create_card);
@@ -74,7 +71,7 @@ describe('Funcionalidade de Card', () => {
 
   it('CT005 - Cenário - Criação de um novo Card no Board com API_TOKEN inválido - Teste Negativo', () => {
 
-    cy.create_a_card(list_id, nome_card, 12345, API_KEY)
+    cy.create_a_card(data_card.list_id, nome_card, 12345, API_KEY)
       .then(response => {
         expect(response.status).to.equal(401)
         expect(response.body).to.equal("invalid app token")
@@ -83,7 +80,7 @@ describe('Funcionalidade de Card', () => {
 
   it('CT006 - Cenário - Criação de um novo Card no Board com API_KEY inválido - Teste Negativo', () => {
 
-    cy.create_a_card(list_id, nome_card, API_TOKEN, 12345)
+    cy.create_a_card(data_card.list_id, nome_card, API_TOKEN, 12345)
       .then(response => {
         expect(response.status).to.equal(401)
         expect(response.body).to.equal("invalid key")
@@ -93,11 +90,11 @@ describe('Funcionalidade de Card', () => {
   it('CT007 - Cenário - Atualizar um Card no Trello - Teste Positivo', () => {
 
 
-    cy.update_card(card_id, board_name, description, API_TOKEN, API_KEY)
+    cy.update_card(card_id, data_card.board_name, data_card.description, API_TOKEN, API_KEY)
       .then(response => {
         expect(response.status).to.equal(200)
-        expect(response.body.name).to.equal(board_name)
-        expect(response.body.desc).to.equal(description)
+        expect(response.body.name).to.equal(data_card.board_name)
+        expect(response.body.desc).to.equal(data_card.description)
         expect(response.body.id).to.eq(card_id); 
         // Validar o schema dos dados de retorno do endpoint.
         expect(response.body).to.be.jsonSchema(schema_get_card);
@@ -105,7 +102,7 @@ describe('Funcionalidade de Card', () => {
   })
 
   it('CT008 - Cenário - Update um Card no Trello com id inválido - Teste Negativo', () => {
-    cy.update_board(0, board_name, description, API_TOKEN, API_KEY)
+    cy.update_board(0, data_card.board_name, data_card.description, API_TOKEN, API_KEY)
       .then(response => {
         expect(response.status).to.equal(400)
         expect(response.body).to.equal("invalid id")
